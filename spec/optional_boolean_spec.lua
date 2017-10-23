ed0f88c84ca387a66c6702bd39bd9dab6c9800ae
Matt Love <matt.love@360dialog.com>
2017-10-23 15:44:30 +0000

    Add test for required boolean members

--- spec/optional_boolean_spec.lua
+++ spec/optional_boolean_spec.lua
@@ -2,24 +2,61 @@
 require 'busted.runner'()
 
 local pb = require"pb"
-local utils = require"utils"
 local opt = require"protos.optional_boolean"
 
-describe("A boolean member", function()
+describe("An optional boolean member", function()
+
     it ("should be detected via HasField", function()
 
         msg = opt.TestOptionalBool()
-        assert.are.equal(false, msg:HasField("bool_"))
+        msg.bool_required = true
+        assert.are.equal(false, msg:HasField("bool_optional"))
+        
+        binary,err = msg:Serialize()
+        assert.is_true(not err)
 
-        msg.bool_ = true;
-        assert.are.equal(true, msg:HasField("bool_"))
+        msg.bool_optional = true
+        assert.are.equal(true, msg:HasField("bool_optional"))
+        binary,err = msg:Serialize()
+        assert.is_true(not err)
 
-        msg.bool_ = false;
-        assert.are.equal(true, msg:HasField("bool_"))
+
+        msg.bool_optional = false
+        assert.are.equal(true, msg:HasField("bool_optional"))
+        binary,err = msg:Serialize()
+        assert.is_true(not err)
+
 
         msg:Clear()
-        assert.are.equal(false, msg:HasField("bool_"))
-
+        msg.bool_required = true;
+        assert.are.equal(false, msg:HasField("bool_optional"))
+        binary,err = msg:Serialize()
+        assert.is_true(not err)
     end)
 
 end)
+
+
+describe("A reqired boolean member", function()
+
+    it ("should be able to be serialized as false", function()
+        msg = opt.TestOptionalBool()
+        msg.bool_required = false
+        assert.are.equal(true, msg:HasField("bool_required"))
+        binary,err = msg:Serialize()
+        assert.is_true(not err)
+        
+    end)
+
+    it ("should be able to be serialized as true", function()
+        msg = opt.TestOptionalBool()
+        msg.bool_required = true
+        assert.are.equal(true, msg:HasField("bool_required"))
+        binary,err = msg:Serialize()
+        assert.is_true(not err)
+        
+    end)
+
+
+end)
+
